2ea390e3ce28212df06f8fa921e35e2830f80efb
JosefinaCresta <josefinacresta@gmail.com>
2022-05-04 07:57:16 +0200

    opportnity Test fail

--- src/main/java/classes/Opportunity.java
+++ src/main/java/classes/Opportunity.java
@@ -42,38 +42,6 @@ public class Opportunity {
         return id;
     }
 
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public Product getProduct() {
-        return product;
-    }
-
-    public void setProduct(Product product) {
-        this.product = product;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
-    public Contact getDecisionMaker() {
-        return decisionMaker;
-    }
-
-    public void setDecisionMaker(Contact decisionMaker) {
-        this.decisionMaker = decisionMaker;
-    }
-
-    public Status getStatus() {
-        return status;
-    }
-
     public void setStatus(Status status) {
         this.status = status;
     }
@@ -88,7 +56,6 @@ public class Opportunity {
         Product product=null;
         int input = readInt("-> ", 3);
         if (input == 1) {
-            //this.setProduct(Product.HYBRID);
             product = Product.HYBRID;
         } else if (input == 2) {
             product = Product.FLATBED;
@@ -99,8 +66,7 @@ public class Opportunity {
         //Set quantity
         int quantity = promptIntWithCheck("Number of products for this Opportunity",  Integer.MAX_VALUE);
 
-
-        //Tiene que agarrar el LEAD al que se le aplicó el metodo de generar oportunidad
+        ///Set decision maker
         Contact lead = leadContact;
 
         //Set status
@@ -133,34 +99,6 @@ public class Opportunity {
             System.out.println("(2) FLATBED");
             System.out.println("(3) BOX");
         }
-
-
-   //Opcion con header y atribute y que dentro lea el int
-  /*  private static int QuantityOptions(String header, String product) {
-        clearConsole();
-        printHeading("Please enter how many product assing to this opportunity");
-        boolean quantitySet = false;
-        int quantity = 0;
-        //TODO Pregunta si está bien pero no le chequea que sea un init positivo. Ver si lo hago con algo del Utils
-        do {
-            printHeading(header);
-            //TODO Entender este.. pero lo que debo hacer es leer un numero entero que escribe Sara
-            //TODO y comprobar que sea entero positivo
-           // quantity = promptIntWithValidation(scanner.next());
-            quantity = Integer.parseInt(scanner.next());
-            //asking the player if he wants to correct his choice
-            clearConsole();
-            printHeading(quantity + product + "is enough for this Opportunity?");
-            System.out.println("(1) Yes!");
-            System.out.println("(2) No, I want to correct it");
-            int input = readInt("-> ", 2);
-            if (input == 1) {
-                quantitySet = true;
-            }
-        } while (!quantitySet);
-
-        return quantity;
-    }*/
 
     private void printStatusMenu(){
         clearConsole();
---
+++ src/test/java/classes/OpportunityTest.java
@@ -0,0 +1,21 @@
+package classes;
+
+import org.junit.jupiter.api.BeforeAll;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OpportunityTest {
+    private Lead lead;
+    private Opportunity opportunity;
+
+    @BeforeAll
+    public void SetUp(){
+        Lead lead = new Lead();
+        Opportunity opportunity = new Opportunity();
+    }
+
+    @org.junit.jupiter.api.Test
+    void createOpportunity() {
+        assertEquals(opportunity, lead.createOpportunity());
+    }
+}
